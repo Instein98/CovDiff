05c17eedea50a13648c5726d5a5d36c76bda42ec
Instein98 <instein98@gmail.com>
2022-03-08 18:14:50 -0600

    fix issues in covdiff2.sh

--- covdiff2.sh
+++ covdiff2.sh
@@ -15,7 +15,7 @@ isMavenProj(){
     # echo Checking whether $id is a maven project...
     pomNum=$(curl "https://api.github.com/repos/$id/contents/" \
         -H "Accept: application/vnd.github.v3+json" \
-        -H "Authorization: Token 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" 2>/dev/null | jq -r '.[].name' | grep pom.xml | wc -l)
+        -H "Authorization: Token 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" 2>/dev/null | jq -r '.[].name' | grep -c pom.xml)
     if [ $pomNum -ge 1 ]; then
         true
     else
@@ -38,9 +38,10 @@ getLibRepoUrl(){
     fixRepoUrl
 }
 
+# input is the url after fix, e.g., 'https://gitbox.apache.org/repos/asf/commons-io.git'
 getRepoAtNameFromUrl(){
     if [[ $url == *"gitbox.apache.org"* ]];then
-        tmp=$(echo $url | sed 's,https://gitbox.apache.org/repos/asf?p=\(.*\).git,\1,')
+        tmp=$(echo $url | sed 's,https://gitbox.apache.org/repos/asf/\(.*\).git,\1,')
         repoAtName=apache@$tmp
     elif [[ $url == *"github.com"* ]]; then
         repoAtName=$(echo "${url##https://github.com/}" | sed 's,/,@,')
@@ -54,7 +55,7 @@ log(){
 
 pwd=$(pwd)
 per_page=100
-page_num=5
+page_num=10
 searchDir=search
 [ ! -d $searchDir ] && mkdir -p $searchDir
 repoDir=repo
@@ -69,10 +70,10 @@ else
     libSearchDir=$searchDir/$libid
     [ ! -d $libSearchDir ] && mkdir -p $libSearchDir
     [ ! -d logs ] && mkdir logs
-    covDiffLog=logs/$libid.log
-    tmp=$(echo "===========$libId===========" | sed 's/./=/g')
+    covDiffLog=$pwd/logs/$libid.log
+    tmp=$(echo "===========$libid===========" | sed 's/./=/g')
     log $tmp
-    log "========== $libId =========="
+    log "========== $libid =========="
     log $tmp
     log ""
 fi
@@ -82,20 +83,23 @@ if [ ! -f $libSearchDir/dependents-all ]; then
     echo Finding the clients using the library...
     echo -n > $libSearchDir/dependents-all
     for ((i=1; i<page_num+1; i++)); do
-        [ -f $libSearchDir/"dependents-p$i" ] && continue
+        if [ -f $libSearchDir/"dependents-p$i" ];then
+            cat "$libSearchDir/dependents-p$i" | jq -r '.[] | "\(.name)@\(.repository_url)@\(.latest_release_number)"' >> $libSearchDir/dependents-all
+            continue
+        fi
         echo "Search for $libid dependents page $i..."
-        curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET "https://libraries.io/api/maven/$libid/dependents?api_key=80be6c2040e9c5266d6c507bdbbcecdb&per_page=$per_page&page=$i" > $libSearchDir/"dependents-p$i"
+        curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET "https://libraries.io/api/maven/$libid/dependents?api_key=80be6c2040e9c5266d6c507bdbbcecdb&per_page=$per_page&page=$i" > $libSearchDir/"dependents-p$i"  2>/dev/null
         while [[ ! $(cat "$libSearchDir/dependents-p$i" | jq -r '.[] | "\(.name)@\(.repository_url)"') ]]; do
             echo "No results found, retrying..."
             sleep 1
-            curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET "https://libraries.io/api/maven/$libid/dependents?api_key=80be6c2040e9c5266d6c507bdbbcecdb&per_page=$per_page&page=$i" > $libSearchDir/"dependents-p$i"
+            curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET "https://libraries.io/api/maven/$libid/dependents?api_key=80be6c2040e9c5266d6c507bdbbcecdb&per_page=$per_page&page=$i" > $libSearchDir/"dependents-p$i"   2>/dev/null
         done
         # cat "$libSearchDir/dependents-p$i" | jq -r '.[] | "\(.name)@\(.repository_url)@\(.versions|last|.number)"' >> $libSearchDir/dependents-all
         cat "$libSearchDir/dependents-p$i" | jq -r '.[] | "\(.name)@\(.repository_url)@\(.latest_release_number)"' >> $libSearchDir/dependents-all
     done
 fi
 
-    # filter the maven projects
+# filter the maven projects
 if [ ! -f $libSearchDir/dependents-maven ]; then
     echo Finding out the clients using maven...
     echo -n > $libSearchDir/dependents-maven
@@ -121,7 +125,7 @@ if [ ! -f $libSearchDir/dependents-version ]; then
         # echo "curl -H" "\"Accept: application/json\"" "-H" "\"Content-Type: application/json\"" "-X GET" "\"https://libraries.io/api/maven/$clientid/$version/dependencies?api_key=80be6c2040e9c5266d6c507bdbbcecdb\"" "2>/dev/null | jq -r" "\".dependencies[] | select(.name==\\\"$libid\\\")|.requirements\""
         result=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET "https://libraries.io/api/maven/$clientid/$version/dependencies?api_key=80be6c2040e9c5266d6c507bdbbcecdb" 2>/dev/null)
         while [[ $result == *"Retry later"* ]]; do
-            echo "Some issues occur, retrying..."
+            # echo "Some issues occur, retrying..."
             result=$(curl -H "Accept: application/json" -H "Content-Type: application/json" -X GET "https://libraries.io/api/maven/$clientid/$version/dependencies?api_key=80be6c2040e9c5266d6c507bdbbcecdb" 2>/dev/null)
         done
         requireVersion=$(echo "$result" | jq -r ".dependencies[] | select(.name==\"$libid\")|.requirements" | grep -v '\$')
@@ -178,72 +182,92 @@ testWithCov(){
     cd $repoDir/$repoAtId
     if [ $# -eq 4 ]; then
         # find the tag
-        tagName=$(git tag -l | grep $checkoutVersion | head -n 1)
+        tagName=$(git tag -l | grep "$checkoutVersion$" | head -n 1)
         if [ ! $tagName ]; then 
-            echo "Failed to find a tag of version $checkoutVersion..." 
+            echo "  Failed to find a tag of version $checkoutVersion..." 
             false
             return
         fi
-        echo "Checking out to tag $tagName (version $checkoutVersion)"
+        echo "  Checking out to tag $tagName (version $checkoutVersion)"
         git checkout tags/$tagName
+
+        echo "  mvn install for $repoAtId..."
+        if [ ! -f "mvninstall-$checkoutVersion.log" ]; then
+            timeout -k 10 15m mvn clean install -Dcheckstyle.skip -Drat.skip=true -l "mvninstall-$checkoutVersion.log"
+            if [ $? -ne 0 ]; then
+                echo "  mvn install failed for $repoAtId"
+                false
+                return 
+            else 
+                echo "  mvn install for $repoAtId succeed"
+            fi
+        else
+            echo "  $repoAtId-$checkoutVersion is already installed"
+        fi
     fi
     # Test the project or return false if it is determined to be timeout or no coverage 
     if [ -f TestTimeOut ] || [ -f NoCoverage ];then
-        echo "TestTimeOut OR NoCoverage!"
+        echo "  TestTimeOut OR NoCoverage!"
         false
         return
     elif [ -f $execName ]; then
-        echo "$execName file found"
+        echo "  $execName file found"
     else
-        echo "Testing...(Timeout is set to 15m)"
+        echo "  Testing...(Timeout is set to 15m)"
         timeout -k 10 15m mvn clean org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent test -Dcheckstyle.skip -Drat.skip=true -l "mvntest.log"
+        
+        # If test timeout
+        if [ $? = "124" ]; then
+            echo "  Test timed out..."
+            touch TestTimeOut
+            false
+            return
+        fi
+
+        # Merge exec files if generating multiple
+        execNum=$(find . -name "jacoco.exec" | wc -l)
+        if [ $execNum = "1" ]; then
+            mv $(find . -name "jacoco.exec") $execName
+        elif [ $execNum = "0" ]; then
+            echo "  No jacoco.exec generated after testing..."
+            touch NoCoverage
+            false
+            return
+        else
+            java -jar $pwd/jacococli.jar merge $(find . -name "jacoco.exec") --destfile=$execName
+        fi
     fi
 
-    # If test timeout
-    if [ $? = "124" ]; then
-        echo "Test timed out..."
-        touch TestTimeOut
-        false
-        return
-    fi
-
-    # Merge exec files if generating multiple
-    execNum=$(find . -name "jacoco.exec" | wc -l)
-    if [ $execNum = "1" ]; then
-        mv $(find . -name "jacoco.exec") $execName
-    elif [ $execNum = "0" ]; then
-        echo "No jacoco.exec generated after testing..."
-        touch NoCoverage
-        false
-        return
-    else
-        java -jar $pwd/jacococli.jar merge $(find . -name "jacoco.exec") --destfile=$execName
-    fi
     cd $pwd
+    echo "  Successfully generate coverage information!"
     true
 }
+
+# Test library for coverage
+getLibRepoUrl  # url <- lib url
+getRepoAtNameFromUrl  # repoAtName <- lib name with @
+libAtName=$repoAtName
+if ! testWithCov "$libAtName" "$url" "$libid-$targetVersion.exec" "$targetVersion"; then
+    echo "Test with cov failed for $libAtName, have to exit..."
+    exit 1
+fi
 
 while read -r record; do
     cd $pwd
     url=${record##* }
-    clientSlashName=${url#"https://github.com/"}
-    clientId=$(echo $clientSlashName | sed 's|/|@|')
+    getRepoAtNameFromUrl  # repoAtName <- client name with @
+    clientId=$repoAtName
+    clientExecName=${clientId//@/:}@$libid.exec
 
-    if ! testWithCov "$clientId" "$url" "$clientId-$libid.exec"; then
+    echo 
+    if ! testWithCov "$clientId" "$url" "$clientExecName"; then
         continue
     fi
 
-    getLibRepoUrl  # url <- lib url
-    getRepoAtNameFromUrl  # repoAtName <- lib name with @
-    libAtName=$repoAtName
-    if ! testWithCov "$libAtName" "$url" "$libid-$targetVersion.exec" "$targetVersion"; then
-        continue
-    fi
-
-    if [ -f $repoDir/$clientId/$clientId-$libid.exec ] && [ -f $repoDir/$libAtName/$libid-$targetVersion.exec ]; then
+    if [ -f $repoDir/$clientId/$clientExecName ] && [ -f $repoDir/$libAtName/$libid-$targetVersion.exec ]; then
         echo "Ready for coverage comparison!"
     else
-        echo "[ERROR] $repoDir/$clientId/$clientId-$libid.exec or $repoDir/$libAtName/$libid-$targetVersion.exec not found..."
+        echo "[ERROR] $repoDir/$clientId/$clientExecName or $repoDir/$libAtName/$libid-$targetVersion.exec not found..."
         continue
     fi
 
@@ -254,7 +278,7 @@ while read -r record; do
     cp -r $pwd/$repoDir/$libAtName/target/classes .
     cp -r $pwd/$repoDir/$libAtName/src/main/java src
     cp $pwd/$repoDir/$libAtName/$libid-$targetVersion.exec lib.exec
-    cp $pwd/$repoDir/$clientId/$clientId-$libid.exec client.exec
+    cp $pwd/$repoDir/$clientId/$clientExecName client.exec
 
     java -jar $pwd/jacococli.jar report lib.exec --classfiles classes  --sourcefiles src --html $pwd/$repoDir/$libAtName/jacoco-$targetVersion --xml $pwd/$repoDir/$libAtName/jacoco-$targetVersion.xml
 
@@ -269,42 +293,5 @@ while read -r record; do
     log "********** Diff Cov Lib:$libAtName Client:$clientId **********"
     log "$diffResult"
     log ""
-
-    # echo
-    # echo Processing $clientId
-    # echo Cloning...
-    # [ ! -d $repoDir/$clientId ] && git submodule add $url $repoDir/$clientId
-
-    # cd $repoDir/$clientId
-    # if [ -f TestTimeOut ] || [ -f NoCoverage ];then
-    #     echo "TestTimeOut OR NoCoverage!"
-    #     continue
-    # elif [ -f $clientId-$libid.exec ]; then
-    #     echo "$clientId-$libid.exec file exists"
-    # else
-    #     echo "Testing...(Timeout is set to 15m)"
-    #     timeout -k 10 15m mvn clean org.jacoco:jacoco-maven-plugin:0.8.7:prepare-agent test -Dcheckstyle.skip -Drat.skip=true -l "mvntest.log"
-    # fi
-
-    # # Move to next client if test timeout
-    # if [ $? = "124" ]; then
-    #     echo "Test timed out! Move to next client..."
-    #     touch TestTimeOut
-    #     continue
-    # fi
-
-    # # Merge exec files if generating multiple
-    # execNum=$(find . -name "jacoco.exec" | wc -l)
-    # echo "Found $execNum exec files"
-    # if [ $execNum = "1" ]; then
-    #     mv $(find . -name "jacoco.exec") $clientId-$libid.exec
-    # elif [ $execNum = "0" ]; then
-    #     echo "NoCoverage! Move to next client..."
-    #     touch NoCoverage
-    #     continue
-    # else
-    #     java -jar $pwd/jacococli.jar merge $(find . -name "jacoco.exec") --destfile=$clientId-$libid.exec
-    # fi
-
     
 done < $libSearchDir/dependents
